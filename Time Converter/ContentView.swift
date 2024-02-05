5c5cc3542aa5fedb166df77925dea8770a014770
Amy C <31007296+Minibeast@users.noreply.github.com>
2024-02-04 22:41:48 -0500

    Initial Commit

---
+++ Time Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Time Converter
+//
+//  Created by Amy C on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time Converter/Time_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ConverterApp.swift
+//  Time Converter
+//
+//  Created by Amy C on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Time_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
